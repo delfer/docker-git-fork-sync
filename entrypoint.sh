15587d726207bd2bd9415785e8b0399c936db2c5
Alexander Chumakov <bestak47@gmail.com>
2017-07-20 17:11:12 +0300

    Moved 'set -e' block

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -u
-set -e
 
 cp -f /ssh/id_rsa /root/.ssh/id_rsa 2> /dev/null
 chmod 600 /root/.ssh/id_rsa 2> /dev/null
 echo "$CRON /sync.sh" > /etc/crontabs/root
 
 if [ ! "$(ls -A $REPO)" ]; then
+  set -e
   git clone $FORK $REPO
   git remote add upstream $UPSTREAM
+  set +e
 fi
 
 /sync.sh
